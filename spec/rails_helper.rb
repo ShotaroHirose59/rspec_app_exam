30be8a0769b2bba553d9f99967460678acabf925
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-28 18:09:51 +0900

    [fix]strftimeではなく、View側の実装を参考にする

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+def short_time(datetime)
+  datetime.strftime("%-m/%d %-H:%M")
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
