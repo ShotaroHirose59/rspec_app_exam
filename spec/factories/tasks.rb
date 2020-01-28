d7d3ed1328ee8c2e7b17cbb6895ff62c2e9913b3
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-28 20:22:56 +0900

    [fix]statusを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     association :project
 
     trait :completed do
-      status { 2 }
+      status { :done }
       completion_date { 'Time.current.yesterday' }
     end
   end
