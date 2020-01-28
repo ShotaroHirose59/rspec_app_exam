c95d3737d92681ef1db540ed0f4144a36903b49b
Shotaro Hirose <shotaro199959@icloud.com>
2020-01-28 18:53:41 +0900

    [fix]実装側で利用しているhelperをincludeして呼べるようにする

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-def short_time(datetime)
-  datetime.strftime("%-m/%d %-H:%M")
-end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -63,4 +60,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+
+  config.include ApplicationHelper
 end
